ac97e4b7b3544164db65a76a3e73bebea18b66f4
roylin <roylin@roylindeMacBook-Air.local>
2026-09-02 06:53:33 +0800

    chore: advance native sandbox integration

--- crates/contracts/tests/dependency_authority.rs
+++ crates/contracts/tests/dependency_authority.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 const ACL_V0_3_SOURCE: &str = "git+https://github.com/A3S-Lab/ACL.git?rev=5317e166222495585909d81f2caffdca90273c99#5317e166222495585909d81f2caffdca90273c99";
 const BOOT_SCHEMA_ADMISSION_SOURCE: &str = "git+https://github.com/A3S-Lab/Boot.git?rev=83d489fb2274ab8e0d277ccd87461cc35c1a9b88#83d489fb2274ab8e0d277ccd87461cc35c1a9b88";
-const CODE_CORE_SOURCE: &str = "git+https://github.com/A3S-Lab/Code.git?rev=a1873e95f4c2cf606630fc5c1d418a8b69196bb4#a1873e95f4c2cf606630fc5c1d418a8b69196bb4";
+const CODE_CORE_SOURCE: &str = "git+https://github.com/A3S-Lab/Code.git?rev=f4dd885e516f359e0259ce79bce43eb6c6f282fe#f4dd885e516f359e0259ce79bce43eb6c6f282fe";
 const CRATES_IO_SOURCE: &str = "registry+https://github.com/rust-lang/crates.io-index";
 const ORM_SCHEMA_ADMISSION_SOURCE: &str = "git+https://github.com/A3S-Lab/ORM.git?rev=52944002dc84b07d88a85f2a4a87f913655e62b5#52944002dc84b07d88a85f2a4a87f913655e62b5";
 
