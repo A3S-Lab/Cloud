629380068da05ed9e0dc2185270083eb63e24567
roylin <roylin@roylindeMacBook-Air.local>
2026-09-05 07:14:57 +0800

    chore(deps): align Cloud with Code zvec release

--- crates/contracts/tests/dependency_authority.rs
+++ crates/contracts/tests/dependency_authority.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 const ACL_V0_3_SOURCE: &str = "git+https://github.com/A3S-Lab/ACL.git?rev=5317e166222495585909d81f2caffdca90273c99#5317e166222495585909d81f2caffdca90273c99";
 const BOOT_SCHEMA_ADMISSION_SOURCE: &str = "git+https://github.com/A3S-Lab/Boot.git?rev=83d489fb2274ab8e0d277ccd87461cc35c1a9b88#83d489fb2274ab8e0d277ccd87461cc35c1a9b88";
-const CODE_CORE_SOURCE: &str = "git+https://github.com/A3S-Lab/Code.git?rev=9e8703405a380508451633eaa8ed1ad59b6515a5#9e8703405a380508451633eaa8ed1ad59b6515a5";
+const CODE_CORE_SOURCE: &str = "git+https://github.com/A3S-Lab/Code.git?rev=c84ba4c84430ff7b58ab2658dc314c031f7cc931#c84ba4c84430ff7b58ab2658dc314c031f7cc931";
 const CRATES_IO_SOURCE: &str = "registry+https://github.com/rust-lang/crates.io-index";
 const ORM_SCHEMA_ADMISSION_SOURCE: &str = "git+https://github.com/A3S-Lab/ORM.git?rev=52944002dc84b07d88a85f2a4a87f913655e62b5#52944002dc84b07d88a85f2a4a87f913655e62b5";
 
